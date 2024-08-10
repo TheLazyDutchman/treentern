5381cff4f5c01a4c405eeeffdd0e1c3fe2871b6a
TheLazyDutchman <jjpouwels2004@gmail.com>
2024-08-10 16:56:21 +0200

    Implement Intern trait for strings

---
+++ src/arena.rs
@@ -0,0 +1,106 @@
+use std::{
+	collections::HashMap,
+	hash::{BuildHasher, Hash},
+	sync::Mutex,
+};
+
+pub struct Arena<T: ?Sized + 'static, H = std::hash::RandomState> {
+	values: Mutex<Vec<&'static T>>,
+	indices: Mutex<HashMap<&'static T, Index, H>>,
+}
+
+#[derive(Clone, Copy)]
+struct Index(usize);
+
+impl<T: ?Sized, H: Default> Default for Arena<T, H> {
+	fn default() -> Self {
+		Self {
+			values: Mutex::default(),
+			indices: Mutex::default(),
+		}
+	}
+}
+
+impl<T: ?Sized> Arena<T> {
+	pub fn new() -> Self {
+		Self::default()
+	}
+}
+
+impl<T: ?Sized, H> Arena<T, H> {
+	fn get(&'static self, index: Index) -> &'static T {
+		self.values.lock().unwrap()[index.0]
+	}
+
+	pub fn insert(&'static self, value: &'static T) -> &'static T
+	where
+		T: Hash + Eq,
+		H: BuildHasher,
+	{
+		if let Some(index) = self
+			.indices
+			.lock()
+			.unwrap()
+			.get(&value)
+		{
+			return self.get(*index);
+		}
+
+		let index = Index(
+			self.values
+				.lock()
+				.unwrap()
+				.len(),
+		);
+		self.values
+			.lock()
+			.unwrap()
+			.push(value);
+		self.indices
+			.lock()
+			.unwrap()
+			.insert(value, index);
+		value
+	}
+
+	pub fn insert_owned(&'static self, value: T) -> &'static T
+	where
+		T: Hash + Eq + Sized,
+		H: BuildHasher,
+	{
+		self.insert(Box::leak(Box::new(value)))
+	}
+}
+
+#[cfg(test)]
+mod test {
+	use crate::Arena;
+
+	#[test]
+	fn insert_arena() {
+		let arena = Box::leak(Box::new(Arena::new()));
+
+		let a = arena.insert_owned(String::from("Hello, World"));
+		let b = arena.insert_owned(String::from("Bonjour"));
+		let c = arena.insert_owned(String::from("Hello, World"));
+
+		assert_ne!(a, b);
+		assert_eq!(a, c);
+
+		assert_eq!(a.as_ptr(), c.as_ptr());
+	}
+
+	#[test]
+	fn unsized_type() {
+		let arena: &Arena<str> = Box::leak(Box::new(Arena::new()));
+
+		let a = arena.insert("Hello, World");
+		let b = arena.insert("Bonjour");
+		let c = arena.insert("Hello, World");
+
+		assert_ne!(a, b);
+		assert_eq!(a, c);
+
+		assert_eq!(a.as_ptr(), c.as_ptr());
+	}
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,86 +1,64 @@
-use std::{
-	cell::RefCell,
-	collections::HashMap,
-	hash::{BuildHasher, Hash},
-	marker::Sized,
-};
+use std::{marker::Sized, sync::LazyLock};
+
+use arena::Arena;
+
+pub mod arena;
 
 pub trait Intern {
-	fn intern(self) -> Interned<Self>
+	fn intern(&'static self) -> Interned<Self>;
+
+	fn intern_owned(self) -> Interned<Self>
 	where
-		Self: Sized;
-}
-
-pub struct Interned<T: 'static>(&'static T);
-
-pub struct Arena<T: 'static, H = std::hash::RandomState> {
-	values: RefCell<Vec<&'static T>>,
-	indices: RefCell<HashMap<&'static T, Index, H>>,
-}
-
-#[derive(Clone, Copy)]
-struct Index(usize);
-
-impl<T, H: Default> Default for Arena<T, H> {
-	fn default() -> Self {
-		Self {
-			values: RefCell::default(),
-			indices: RefCell::default(),
-		}
-	}
-}
-
-impl<T> Arena<T> {
-	pub fn new() -> Self {
-		Self::default()
-	}
-}
-
-impl<T, H> Arena<T, H> {
-	fn get(&'static self, index: Index) -> &'static T {
-		self.values.borrow()[index.0]
-	}
-
-	pub fn insert(&'static self, value: T) -> &'static T
-	where
-		T: Hash + Eq,
-		H: BuildHasher,
+		Self: Sized,
 	{
-		if let Some(index) = self
-			.indices
-			.borrow()
-			.get(&value)
-		{
-			return self.get(*index);
-		}
+		Box::leak(Box::new(self)).intern()
+	}
+}
 
-		let index = Index(self.values.borrow().len());
-		let value = Box::leak(Box::new(value));
-		self.values
-			.borrow_mut()
-			.push(value);
-		self.indices
-			.borrow_mut()
-			.insert(value, index);
-		value
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct Interned<T: ?Sized + 'static>(&'static T);
+
+static STRING_ARENA: LazyLock<Arena<String>> = LazyLock::new(Arena::new);
+
+impl Intern for String {
+	fn intern(&'static self) -> Interned<Self> {
+		Interned(STRING_ARENA.insert(self))
+	}
+}
+
+static STR_ARENA: LazyLock<Arena<str>> = LazyLock::new(Arena::new);
+
+impl Intern for str {
+	fn intern(&'static self) -> Interned<Self> {
+		Interned(STR_ARENA.insert(self))
 	}
 }
 
 #[cfg(test)]
 mod test {
-	use crate::Arena;
+	use crate::Intern;
 
 	#[test]
-	fn insert_arena() {
-		let arena = Box::leak(Box::new(Arena::new()));
-
-		let a = arena.insert(String::from("Hello, World"));
-		let b = arena.insert(String::from("Bonjour"));
-		let c = arena.insert(String::from("Hello, World"));
+	fn intern_string() {
+		let a = String::from("Hello, World").intern_owned();
+		let b = String::from("Bonjour").intern_owned();
+		let c = String::from("Hello, World").intern_owned();
 
 		assert_ne!(a, b);
 		assert_eq!(a, c);
 
-		assert_eq!(a.as_ptr(), c.as_ptr());
+		assert_eq!(a.0.as_ptr(), c.0.as_ptr());
+	}
+
+	#[test]
+	fn intern_str() {
+		let a = "Hello, World".intern();
+		let b = "Bonjour".intern();
+		let c = "Hello, World".intern();
+
+		assert_ne!(a, b);
+		assert_eq!(a, c);
+
+		assert_eq!(a.0.as_ptr(), c.0.as_ptr());
 	}
 }
